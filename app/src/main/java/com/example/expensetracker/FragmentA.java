c3b61d362abeed6806e3e045e1266d7cea6f8db5
Santosh Kandalkar <skandalkar1508@gmail.com>
2024-09-06 18:51:33 +0530

    Modification and adding of the documentation about uses of collection of user insights through the survey.

--- app/src/main/java/com/example/expensetracker/FragmentA.java
+++ app/src/main/java/com/example/expensetracker/FragmentA.java
@@ -9,56 +9,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentA#newInstance} factory method to
- * create an instance of this fragment.
+
+ In this Fragment the collection of user's answer for survey is done then passed to the user's
+ respective database created at RealtimeDatabase to improve or enhance the user experience this
+ require #Backend_expense_track
+
  */
-public class FragmentA extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class FragmentA extends Fragment
+{
 
     public FragmentA() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentA.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentA newInstance(String param1, String param2) {
-        FragmentA fragment = new FragmentA();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
 }
