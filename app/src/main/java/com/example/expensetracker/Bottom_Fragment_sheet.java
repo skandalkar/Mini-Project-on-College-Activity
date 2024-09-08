fb8b486fd154715958758ee748ee6535f8cef991
Santosh Kandalkar <skandalkar1508@gmail.com>
2024-09-08 23:37:17 +0530

    required changes done, floating action buton with bottomsheetdialg done.

---
+++ app/src/main/java/com/example/expensetracker/Bottom_Fragment_sheet.java
@@ -0,0 +1,27 @@
+package com.example.expensetracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class Bottom_Fragment_sheet extends BottomSheetDialogFragment {
+
+    public Bottom_Fragment_sheet() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view =  inflater.inflate(R.layout.fragment_bottom__sheet, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/expensetracker/FragmentHome.java
+++ app/src/main/java/com/example/expensetracker/FragmentHome.java
@@ -9,12 +9,17 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import org.w3c.dom.Text;
 
 public class FragmentHome extends Fragment {
 
     Toolbar toolbar;
     TextView userProfile_Name;
+
+    FloatingActionButton floatingActionButton;
+
     public FragmentHome() {
         // Required empty public constructor
     }
@@ -29,6 +34,18 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        floatingActionButton = view.findViewById(R.id.addexpense);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bottom_Fragment_sheet bottomFragmentSheet = new Bottom_Fragment_sheet();
+                bottomFragmentSheet.show(getParentFragmentManager(),bottomFragmentSheet.getTag());
+            }
+        });
+
+
        /* toolbar = view.findViewById(R.id.home_toolbar);
         userProfile_Name = view.findViewById(R.id.user_profile_name);
         String userProfileName = getArguments().getString("username");
